0aa4782a5df5a2de58363c32c86dae147b7efb7d
mendiolanivelle <mendiolanivelle@gmail.com>
2018-02-11 20:57:17 +0800

    asdfsdf

--- Build/Android/src/LeftHand/Deadly_Snow_Balls/OBBData.java
+++ Build/Android/src/LeftHand/Deadly_Snow_Balls/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-76259133L // the length of the file in bytes
+78332484L // the length of the file in bytes
 )
 };
 };
